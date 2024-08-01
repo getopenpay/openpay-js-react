dcda6d89dfa5f6aebbfd6cba057b9432fce9f8d9
Jared Dantis <jareddantis@gmail.com>
2024-08-01 19:31:36 +0800

    Bootstrap example NextJS site

---
+++ example/src/app/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { OpenPayElementsProvider, CardElement } from '@getopenpay/openpay-js-react';
+
+export default function Home() {
+  return (
+    <OpenPayElementsProvider>
+      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <div className='w-full max-w-5xl font-mono flex flex-col gap-4'>
+          <h1 className='text-2xl font-bold'>OpenPay Elements</h1>
+          <p className='text-lg'>Accept payments through OpenPay, right on your site</p>
+          <hr />
+
+          <h2 className='text-xl font-bold'>Card element</h2>
+          <CardElement />
+        </div>
+      </main>
+    </OpenPayElementsProvider>
+  );
+}
