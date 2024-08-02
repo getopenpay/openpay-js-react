2dbb072426ca8e78503628ed05507a036918bdd2
Jared Dantis <jareddantis@gmail.com>
2024-08-02 19:35:50 +0800

    vite: Remove logging on prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   build: {
     copyPublicDir: false,
     lib: {
