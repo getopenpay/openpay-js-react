88e766b42fdc6128d83de9262ae4ca00790807c2
Jared Dantis <jared@getopenpay.com>
2024-08-12 13:03:33 +0800

    example: Delete old pages

--- example/src/app/elements/_components/InputField.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, PropsWithChildren } from 'react';
-
-export const InputField: FC<PropsWithChildren> = (props) => {
-  const { children } = props;
-
-  return <div className="bg-emerald-50 dark:bg-emerald-800 shadow-md px-4 py-2 rounded-md my-2">{children}</div>;
-};
